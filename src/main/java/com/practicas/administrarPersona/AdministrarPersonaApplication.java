b9e767efb5821659c1448ec637549471f76015ad
Octavio Ramses Cardona Ortiz <cardonaortiz.octavio@gmail.com>
2024-12-05 12:31:08 -0600

    Inicializando reepositorio

---
+++ src/main/java/com/practicas/administrarPersona/AdministrarPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.practicas.administrarPersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrarPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrarPersonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicas/administrarPersona/AdministrarPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicas.administrarPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrarPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
