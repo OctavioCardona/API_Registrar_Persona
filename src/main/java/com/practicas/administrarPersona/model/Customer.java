8a9f5720957b2790ef79e0239168b173aacdc56e
Octavio Ramses Cardona Ortiz <cardonaortiz.octavio@gmail.com>
2024-12-17 18:01:21 -0600

    Actualizando pom y cambiando getters y setters definidos por anotaciones lombok

--- src/main/java/com/practicas/administrarPersona/model/Customer.java
+++ src/main/java/com/practicas/administrarPersona/model/Customer.java
@@ -1,10 +1,19 @@
 package com.practicas.administrarPersona.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
 /**
  * Class to create customer.
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Customer {
     /**
      * Name of the customer
@@ -20,38 +29,6 @@ public class Customer {
      * Birthday date of customer.
      */
     private LocalDate birthday;
-
-    public Customer (){}
-
-    public Customer (String name, Integer age, LocalDate birthday){
-        this.name = name;
-        this.age = age;
-        this.birthday = birthday;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
 
     /**
      * Method to override the response of toString method
